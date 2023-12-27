03e47c5ea6d3f175f8e21e255def4f6f0cf648c0
dalton-0x0 <12995327+dalton-0x0@users.noreply.github.com>
2023-12-27 15:06:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+# Data types
+
+# String
+my_string = "Kelly Rowland"
+first_char = my_string[0]
+last_char = my_string[-1]
+print(f"The first character is: {first_char}")
+print("The last character is: ", last_char)
+
+# Integer
+my_int = 123
+
+# Boolean
+my_bool = True
+
+# Float
+my_float = 3.14
+
+# List
+my_list = [1, 2, 3, 4, 5]
+
+# Dictionary
+my_dict = {"name": "Kelly", "age": 30}
+
+# Tuple
+my_tuple = (1, 2, 3, 4, 5)
+
+# Printing the data types
+print("String: ", my_string)
+print("Integer: ", my_int)
+print("Boolean: ", my_bool)
